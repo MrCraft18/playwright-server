12d04a3d15e5ff12146d03d0f14e72e2d476d6f8
MrCraft18 <mariocaden@gmail.com>
2025-04-05 16:25:01 +0900

    make not headless for now

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { chromium } from "playwright";
 import { configDotenv } from 'dotenv'; configDotenv()
 
 const browser = await chromium.launchServer({
-    headless: true,
+    headless: false,
     port: 3501,
     wsPath: process.env.HOST
 })
