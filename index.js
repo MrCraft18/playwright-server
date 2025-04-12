85a5c26ef551a72572b908c6d4f35e2ce397afb8
MrCraft18 <mariocaden@gmail.com>
2025-04-12 03:40:51 -0400

    make headless based on env var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { chromium } from "playwright";
 import { configDotenv } from 'dotenv'; configDotenv()
 
 const browser = await chromium.launchServer({
-    headless: false,
+    headless: process.env.HEADLESS,
     port: 3501,
     wsPath: process.env.HOST
 })
