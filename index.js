f6ee689219d672520da09abf9db727264670c882
MrCraft18 <mariocaden@gmail.com>
2025-04-12 03:45:29 -0400

    it actually works this way

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { chromium } from "playwright";
 import { configDotenv } from 'dotenv'; configDotenv()
 
 const browser = await chromium.launchServer({
-    headless: process.env.HEADLESS,
+    headless: process.env.HEADLESS === "true" ? true : false,
     port: 3501,
     wsPath: process.env.HOST
 })
